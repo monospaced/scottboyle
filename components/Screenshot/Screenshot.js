ccab941f8db234f5008efe9cf2b198955d5433cf
Scott Boyle <scott@monospaced.com>
2017-01-02 01:08:23 +0000

    Image load transition

--- components/Screenshot/Screenshot.js
+++ components/Screenshot/Screenshot.js
@@ -1,25 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Screenshot.css';
 
-const Screenshot = ({slug, project: {link, title}}) => {
-  const image = require(`../../assets/${slug}.png`);
-  const img = (
-    <img
-      className="Screenshot"
-      alt={title}
-      width="400"
-      height={`${(image.width > 398 ? image.height / 2 : image.height) + 2}`}
-      src={`/assets/${slug}.png`}
-    />
-  );
-  if (link) {
-    return (
-      <a href={link}>
-        {img}
-      </a>
-    );
+class Screenshot extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loaded: true,
+    };
+    this.imageWillLoad = this.imageWillLoad.bind(this);
   }
-  return img;
-};
+  render() {
+    const {slug, project: {link, title}} = this.props;
+    const image = require(`../../assets/${slug}.png`);
+    const img = (
+      <img
+        className={`Screenshot${!this.state.loaded ? ' is-loading' : ''}`}
+        alt={title}
+        width="400"
+        height={`${(image.width > 398 ? image.height / 2 : image.height) + 2}`}
+        src={`/assets/${slug}.png`}
+      />
+    );
+    if (link) {
+      return (
+        <a href={link}>
+          {img}
+        </a>
+      );
+    }
+    return img;
+  }
+  componentDidMount() {
+    this.imageWillLoad();
+  }
+  componentWillReceiveProps(nextProps) {
+    if (nextProps !== this.props) {
+      this.imageWillLoad(nextProps);
+    }
+  }
+  imageWillLoad(nextProps) {
+    const {slug} = nextProps || this.props;
+    const img = document.createElement('img');
+    this.setState({loaded: false});
+    img.onload = () => {
+      this.setState({loaded: true});
+    };
+    img.src = `/assets/${slug}.png`;
+  }
+}
 
 export default Screenshot;
