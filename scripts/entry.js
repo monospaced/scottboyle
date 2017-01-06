ca7c713bfcb89a6769e2adef8add4d283ad4ef1a
Scott Boyle <scott@monospaced.com>
2017-01-06 11:16:31 +0000

    Document entry render methods

--- scripts/entry.js
+++ scripts/entry.js
@@ -3,18 +3,20 @@ import ReactDOM from'react-dom';
 import ReactDOMServer from'react-dom/server';
 import {Router, browserHistory, createMemoryHistory} from 'react-router';
 import DocumentMeta from 'react-document-meta';
-import Routes from './routes';
 import Root from '../components/Root/Root.js';
+import routes from './routes';
 
 if (typeof document !== 'undefined') {
+  // Single page app
   const app = document.getElementById('app');
-  ReactDOM.render(<Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory} routes={Routes}/>, app);
+  ReactDOM.render(<Router onUpdate={() => window.scrollTo(0, 0)} history={browserHistory} routes={routes}/>, app);
 }
 
 export default (locals, callback) => {
+  // Render static site, consts must be declared in this order
   const history = createMemoryHistory(locals.path);
-  const app = {__html: ReactDOMServer.renderToString(<Router history={history} routes={Routes}/>)};
+  const app = {__html: ReactDOMServer.renderToString(<Router history={history} routes={routes}/>)};
   const meta = DocumentMeta.renderAsReact();
-  const html = ReactDOMServer.renderToStaticMarkup(<Root meta={meta} app={app}/>);
+  const html = ReactDOMServer.renderToStaticMarkup(<Root app={app} meta={meta}/>);
   callback(null, '<!DOCTYPE html>' + html);
 };
