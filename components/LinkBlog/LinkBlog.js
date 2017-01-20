fd2284814624f866edf39e6910b4bfd5df6b58af
Scott Boyle <scott@monospaced.com>
2017-01-20 00:12:33 +0000

    Add stub for JS rendered link blog

--- components/LinkBlog/LinkBlog.js
+++ components/LinkBlog/LinkBlog.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOMServer from'react-dom/server';
 import DocumentMeta from 'react-document-meta';
-import marked from 'marked';
 import './LinkBlog.css';
 
 class LinkBlog extends Component {
   constructor(props) {
     super(props);
+    this.state = { spa: false };
+  }
+  componentDidMount() {
+    this.setState({ spa: true });
   }
   render() {
     const {data} = this.props;
@@ -18,18 +21,25 @@ class LinkBlog extends Component {
         />
         <section>
           <h2>Link blog</h2>
+          {!this.state.spa ?
           <div className="LinkBlog" dangerouslySetInnerHTML={{__html: `
-<?php
-  $ch = curl_init();
-  $timeout = 2;
-  curl_setopt($ch, CURLOPT_URL,
-  "http://app.feed.informer.com/digest3/JDTPAOKUAA.html");
-  curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_exec($ch);
-  curl_close($ch);
-?>
-            `.trim()}}
-          />
+            <?php
+              $ch = curl_init();
+              $timeout = 2;
+              curl_setopt($ch, CURLOPT_URL,
+              "http://app.feed.informer.com/digest3/JDTPAOKUAA.html");
+              curl_setopt($ch, CURLOPT_HEADER, 0);
+              curl_exec($ch);
+              curl_close($ch);
+            ?>
+          `.trim()}}
+          /> :
+          <div className="LinkBlog">
+            <ul>
+              <li>Links here</li>
+            </ul>
+          </div>
+        }
         </section>
       </main>
     );
