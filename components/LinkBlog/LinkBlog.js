f79a6cdc38c2ec81751543239600788c5aaeccef
Scott Boyle <scott@monospaced.com>
2017-01-21 19:31:01 +0000

    Isomorphic links from pinboard jsonp

--- components/LinkBlog/LinkBlog.js
+++ components/LinkBlog/LinkBlog.js
@@ -1,43 +1,81 @@
 import React, {Component} from 'react';
 import ReactDOMServer from'react-dom/server';
 import DocumentMeta from 'react-document-meta';
+import fetchJsonp from 'fetch-jsonp';
 import './LinkBlog.css';
 
 class LinkBlog extends Component {
   constructor(props) {
     super(props);
-    this.state = { spa: false };
+    this.state = {
+      links: false,
+    };
   }
   componentDidMount() {
-    this.setState({ spa: true });
+    fetchJsonp('https://feeds.pinboard.in/json/v1/u:monospaced/?count=40&cb=pinboard', {
+      jsonpCallback: 'cb',
+    })
+    .then(res => {
+      return res.json();
+    })
+    .then(json => {
+      return this.setState({
+        links: json,
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      return this.setState({
+        links: false,
+      });
+    });
   }
   render() {
     const {data} = this.props;
     return (
       <main className="Main">
         <DocumentMeta
-          title={`Link blog | ${data.title} ${data.subtitle}`}
+          title={`Link Blog | ${data.title} ${data.subtitle}`}
           description={data.description}
         />
         <section>
-          <h2>Link blog</h2>
-          {!this.state.spa ?
+          <h2>Link Blog</h2>
+          {!this.state.links ?
           <div className="LinkBlog" dangerouslySetInnerHTML={{__html: `
-<?php
+<ul><?php
+  function jsonp_decode($jsonp, $assoc = false) {
+    if($jsonp[0] !== '[' && $jsonp[0] !== '{') {
+      $jsonp = substr($jsonp, strpos($jsonp, '('));
+    }
+    return json_decode(trim($jsonp,'();'), $assoc);
+  }
   $ch = curl_init();
-  $timeout = 2;
   curl_setopt($ch, CURLOPT_URL,
-  "https://feed.informer.com/widgets/2JILLJFOJU.html");
+  "https://feeds.pinboard.in/json/v1/u:monospaced/?count=40&cb=pinboard");
   curl_setopt($ch, CURLOPT_HEADER, 0);
-  curl_exec($ch);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  $res = curl_exec($ch);
   curl_close($ch);
-?>
+  $links = jsonp_decode($res, true);
+  foreach($links as $item) {
+?><li><a href="<?php
+  echo $item[u]
+?>"><?php
+  echo $item[d]
+?></a> <small><?php
+  echo $item[dt]
+?></small></a></li><?php
+  }
+?></ul>
           `.trim()}}/> :
           <div className="LinkBlog">
-            {/*
-              <script src="https://feed.informer.com/widgets/JDTPAOKUAA.js"/>
-              https://feed.informer.com/digests/4TVQEYQXTS/feeder.rss
-            */}
+            <ul>{this.state.links.map((item) => {
+              return (
+                <li key={item.dt}>
+                  <a href={item.u}>{item.d}</a> <small>{item.dt}</small>
+                </li>
+              );
+            })}</ul>
           </div>
         }
         </section>
