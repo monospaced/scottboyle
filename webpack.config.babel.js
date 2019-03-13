3cf6a45099a91db83eb3ec89b941f6a2f926ccdf
monospaced <scott@monospaced.com>
2019-03-13 15:40:59 +0000

    build: upgrade webpack

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,81 +1,76 @@
-import ExtractTextPlugin from "extract-text-webpack-plugin";
 import ReactRouterToArray from "react-router-to-array";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import OptimizeCSSAssetsPlugin from "optimize-css-assets-webpack-plugin";
 import StaticSiteGeneratorPlugin from "static-site-generator-webpack-plugin";
+import UglifyJsPlugin from "uglifyjs-webpack-plugin";
 import webpack from "webpack";
 
-const production = process.env.NODE_ENV === "production";
+module.exports = () => {
+  // Prevent webpack from attempting to process css before loaders are configured
+  require.extensions[".css"] = () => {};
 
-// Prevent webpack from attempting to process css before loaders are configured
-require.extensions[".css"] = () => {
-  return;
-};
+  // Routes array is required in config.plugins
+  const routes = ReactRouterToArray(require("./src/scripts/routes"));
 
-// Routes array is required in config.plugins
-const routes = ReactRouterToArray(require("./src/scripts/routes"));
-
-const config = {
-  devServer: { inline: false, stats: "minimal" },
-  entry: "./src/scripts/entry.js",
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel-loader",
-        query: { presets: ["env", "react"] },
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract("css-loader!postcss-loader"),
-      },
-      {
-        test: /\.woff$/,
-        loader: "url-loader?mimetype=application/font-woff",
-      },
-      {
-        test: /\.(jpg)$/,
-        loader: "file-loader",
-        options: { name: "assets/[name].[ext]" },
-      },
-      {
-        test: /\.htaccess$/,
-        loader: "file-loader",
-        options: { name: "[name]" },
-      },
-      {
-        test: /\.(ico|png|svg|webmanifest|xml)$/,
-        loader: "file-loader",
-        options: { name: "[name].[ext]" },
-      },
-      {
-        test: /\.json$/,
-        loader: "json-loader",
-      },
+  const config = {
+    devServer: { historyApiFallback: true, inline: false, stats: "minimal" },
+    entry: "./src/scripts/entry.js",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+          query: { presets: ["env", "react"] },
+        },
+        {
+          test: /\.css$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            { loader: "css-loader", options: { importLoaders: 1 } },
+            "postcss-loader",
+          ],
+        },
+        {
+          test: /\.woff$/,
+          loader: "url-loader?mimetype=application/font-woff",
+        },
+        {
+          test: /\.(jpg)$/,
+          loader: "file-loader",
+          options: { name: "assets/[name].[ext]" },
+        },
+        {
+          test: /\.htaccess$/,
+          loader: "file-loader",
+          options: { name: "[name]" },
+        },
+        {
+          test: /\.(ico|png|svg|webmanifest|xml)$/,
+          loader: "file-loader",
+          options: { name: "[name].[ext]" },
+        },
+      ],
+    },
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({ cache: true, parallel: true }),
+        new OptimizeCSSAssetsPlugin(),
+      ],
+    },
+    output: {
+      filename: "bundle.js",
+      globalObject: "this",
+      libraryTarget: "umd",
+      path: __dirname + "/build",
+      publicPath: "/",
+    },
+    plugins: [
+      new MiniCssExtractPlugin({ filename: "styles.css" }),
+      new StaticSiteGeneratorPlugin("bundle.js", routes),
     ],
-  },
-  output: {
-    filename: "bundle.js",
-    libraryTarget: "umd",
-    path: __dirname + "/build",
-    publicPath: "/",
-  },
-  plugins: [
-    new ExtractTextPlugin("styles.css"),
-    new StaticSiteGeneratorPlugin("bundle.js", routes),
-  ],
-  stats: "minimal",
+    stats: "minimal",
+  };
+
+  return config;
 };
-
-if (production) {
-  config.plugins = [
-    ...config.plugins,
-    // Put react into production mode
-    new webpack.DefinePlugin({ "process.env": { NODE_ENV: `'production'` } }),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: { warnings: false },
-      output: { comments: false },
-    }),
-  ];
-}
-
-module.exports = config;
