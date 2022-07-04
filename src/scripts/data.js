f2b007287f92fb1b6ee9ee2d830667f52f627aba
monospaced <scott@monospaced.com>
2022-07-04 23:03:38 +0100

    feat: align with LinkedIn

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -1,8 +1,7 @@
 module.exports = {
   title: "Scott Boyle",
   subtitle: "Portfolio",
-  description:
-    "Portfolio for Scott Boyle, freelance web developer based in London, UK.",
+  description: "Portfolio for Scott Boyle, web developer based in London, UK.",
   about: `
 <a class="fn uid url" href="https://scottboyle.uk">Scott Boyle</a> is a <span class="category">web professional</span> <span class="note">specialising in modern front-end development</span>. Scott lives in <span class="adr"><span class="locality">London</span> <span class="country-name">UK</span></span> and runs <span class="vcard"><a href="https://monospaced.com" class="fn org url">Monospaced</a></span>, a web software consultancy working with clients in London and around the&nbsp;world.
 
@@ -19,6 +18,7 @@ module.exports = {
 * <a href="https://github.com/monospaced" rel="me">Github</a>
 * <a href="https://www.flickr.com/photos/scottboyle/">Flickr</a>
 * <a href="https://www.instagram.com/mono_spaced/" rel="me">Instagram</a>
+* <a href="https://www.linkedin.com/in/scottboyle/" rel="me">LinkedIn</a>
 * <a href="https://twitter.com/mono_spaced" rel="me">Twitter</a>
   `,
   projects: {
@@ -50,8 +50,8 @@ Migrated Hubble’s site from AngularJS to React and architected a UI system to 
     },
     refinitiv: {
       title: "Refinitiv",
-      date: "2019",
-      link: "https://www.refinitiv.com",
+      date: "2018–2019",
+      link: "https://forge.refinitiv.com",
       client: null,
       image: {
         width: 398,
@@ -63,12 +63,8 @@ Created a CSS webpack build solution to rebrand the Thomson Reuters web platform
     },
     "thomson-reuters": {
       title: "Thomson Reuters",
-      date: "2018–2019",
+      date: "2017–2019",
       link: "https://www.thomsonreuters.com/en.html",
-      client: {
-        title: "Bear and Rocket",
-        link: "",
-      },
       image: {
         width: 398,
         height: 301,
@@ -79,7 +75,7 @@ Building a design system and React component&nbsp;library.
     },
     "social-360": {
       title: "Social 360",
-      date: "2015–2018",
+      date: "2014–2018",
       link: "",
       client: {
         title: "Profit From Play",
@@ -90,9 +86,9 @@ Building a design system and React component&nbsp;library.
         height: 310,
       },
       content: `
-Complete build and project delivery of complex single-page web app. Highcharts data visualisation, Redux state management and Jest unit&nbsp;testing.
+Complete build and project delivery of a complex single-page web application. Highcharts data visualisation, Redux state management and Jest unit&nbsp;testing.
 
-Also full marketing website build, front end and CMS&nbsp;(Perch).
+Also a full marketing website build, front end and CMS&nbsp;(Perch).
       `,
     },
     omron: {
@@ -115,10 +111,6 @@ Assisted Hanno with front-end development of Evolv&nbsp;microsite.
       title: "Compass Group",
       date: "2017",
       link: "",
-      client: {
-        title: "Angular Ghost",
-        link: "",
-      },
       image: {
         width: 398,
         height: 244,
@@ -134,8 +126,8 @@ UI design and complete front-end build, Angular NgRx web&nbsp;app.
       date: "2017",
       link: "",
       client: {
-        title: "Freeform",
-        link: "",
+        title: "Profit From Play",
+        link: "https://profitfromplay.com",
       },
       image: {
         width: 398,
@@ -143,6 +135,22 @@ UI design and complete front-end build, Angular NgRx web&nbsp;app.
       },
       content: `
 Complete front-end build, React/Webpack static site&nbsp;generator.
+      `,
+    },
+    traq: {
+      title: "Traq",
+      date: "2015–2017",
+      link: "",
+      client: {
+        title: "Profit From Play",
+        link: "https://profitfromplay.com",
+      },
+      image: {
+        width: 398,
+        height: 308,
+      },
+      content: `
+Technical specifications and complete AngularJS front-end build. Stripe payments integration, CI and AWS infrastructure management, PCI compliance. Working in close collaboration with .NET API developers and the product&nbsp;team.
       `,
     },
     freeform: {
@@ -158,12 +166,12 @@ Complete front-end build, React/Webpack static site&nbsp;generator.
         height: 241,
       },
       content: `
-Building full stack  prototypes and technical experiments utilising a variety of  technologies. Node.js, React, Angular 1 and 2, Ionic. OpenCV, Leap Motion, Alexa Skills, Facebook Messenger Platform,&nbsp;etc.
+Building full stack  prototypes and technical experiments utilising a variety of  technologies. Node.js, React, AngularJS and Angular, Ionic. OpenCV, Leap Motion, Alexa Skills, Facebook Messenger Platform,&nbsp;etc.
       `,
     },
-    traq: {
-      title: "Traq",
-      date: "2015–2016",
+    "just-eat": {
+      title: "Just Eat",
+      date: "2011–2016",
       link: "",
       client: {
         title: "Profit From Play",
@@ -171,10 +179,10 @@ Building full stack  prototypes and technical experiments utilising a variety of
       },
       image: {
         width: 398,
-        height: 308,
+        height: 311,
       },
       content: `
-Technical specifications and complete Angular 1 front-end build. Stripe payments integration, CI and AWS infrastructure management, PCI compliance. Working in close collaboration with .NET API developers and the product&nbsp;team.
+Front-end template builds for responsive homepage project, mobile app prototypes, and website&nbsp;refresh.
       `,
     },
     nhs: {
@@ -195,25 +203,9 @@ Technical specifications and complete Angular 1 front-end build. Stripe payments
 Managed the delivery of this content&nbsp;website.
       `,
     },
-    "just-eat": {
-      title: "Just Eat",
-      date: "2014",
-      link: "",
-      client: {
-        title: "Profit From Play",
-        link: "https://profitfromplay.com",
-      },
-      image: {
-        width: 398,
-        height: 311,
-      },
-      content: `
-Front-end template builds for responsive homepage project, mobile app prototypes, and website&nbsp;refresh.
-      `,
-    },
     "find-my-past": {
       title: "Find My Past",
-      date: "2013",
+      date: "2013–2014",
       link: "",
       client: {
         title: "Profit From Play",
@@ -225,6 +217,24 @@ Front-end template builds for responsive homepage project, mobile app prototypes
       },
       content: `
 Front-end design and build for a family tree web app built in AngularJS. Complex diagram views built completely in&nbsp;CSS.
+      `,
+    },
+    "dow-jones": {
+      title: "Dow Jones",
+      date: "2008–2013",
+      link: "",
+      client: {
+        title: "Profit From Play",
+        link: "https://profitfromplay.com",
+      },
+      image: {
+        width: 199,
+        height: 352,
+      },
+      content: `
+### Financial News
+
+Complete front-end templates for redesign of efinancialnews.com. Semantic HTML and object-oriented CSS produced a lightweight, accessible and SEO friendly&nbsp;site.
       `,
     },
     bilbary: {
@@ -292,10 +302,6 @@ Lead front-end developer for portal website aimed at the global Chinese professi
       title: "Stordis",
       date: "2011",
       link: "",
-      client: {
-        title: "Age of Peers",
-        link: "https://ageofpeers.com",
-      },
       image: {
         width: 199,
         height: 231,
@@ -442,40 +448,6 @@ Front-end templating for a successful redesign, taking OneRiot out of beta. Obje
 Part of a 5 person team building the front end for Best Buy’s UK site. Particular focus on performance, object-oriented CSS, and asynchronous JavaScript&nbsp;loading.
       `,
     },
-    "dow-jones": {
-      title: "Dow Jones",
-      date: "2009",
-      link: "",
-      client: {
-        title: "Profit From Play",
-        link: "https://profitfromplay.com",
-      },
-      image: {
-        width: 199,
-        height: 352,
-      },
-      content: `
-### Financial News
-
-Complete front-end templates for redesign of efinancialnews.com. Semantic HTML and object-oriented CSS produced a lightweight, accessible and SEO friendly&nbsp;site.
-      `,
-    },
-    ask: {
-      title: "Ask",
-      date: "2008",
-      link: "",
-      client: {
-        title: "Profit From Play",
-        link: "https://profitfromplay.com",
-      },
-      image: {
-        width: 199,
-        height: 112,
-      },
-      content: `
-Rapid agile prototyping of interactive behaviours and animations. Adaptive/elastic layouts, visual design candidates and user&nbsp;skins.
-      `,
-    },
     pepsi: {
       title: "Pepsi",
       date: "2008",
@@ -492,9 +464,25 @@ Rapid agile prototyping of interactive behaviours and animations. Adaptive/elast
 ### Pepsi MaxCast
       `,
     },
+    ask: {
+      title: "Ask",
+      date: "2007–2008",
+      link: "",
+      client: {
+        title: "Profit From Play",
+        link: "https://profitfromplay.com",
+      },
+      image: {
+        width: 199,
+        height: 112,
+      },
+      content: `
+Rapid prototyping of interactive behaviours and animations. Adaptive/elastic layouts, visual design candidates and user&nbsp;skins.
+      `,
+    },
     "financial-careers": {
       title: "Financial Careers",
-      date: "2007",
+      date: "2007–2008",
       link: "",
       client: {
         title: "Profit From Play",
