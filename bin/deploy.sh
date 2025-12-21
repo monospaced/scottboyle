2d551cc9782a8e88fc0cd353972a849ebbc5093b
monospaced <scott@monospaced.com>
2025-12-21 12:15:12 +0000

    build: update deps

--- bin/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DEST="~/webapps/static_scottboyle";
-HOST="monospaced";
-
-rsync --delete --exclude={.well-known,google26897385c23df8ed.html} --recursive --rsh=ssh ./build/$1 $HOST:$DEST && echo "Deployed to $HOST:$DEST/$1"
