ed203465e433b75a03a20a698dd4d0f03f6c2dd7
monospaced <scott@monospaced.com>
2025-12-22 13:33:05 +0000

    feat: update assets and data

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -1,54 +1,64 @@
 import React from "react";
 
-import "../../assets/android-chrome-192x192.png";
-import "../../assets/android-chrome-512x512.png";
 import "../../assets/apple-touch-icon.png";
-import "../../assets/browserconfig.xml";
 import "../../assets/favicon.ico";
-import "../../assets/favicon-16x16.png";
-import "../../assets/favicon-32x32.png";
+import "../../assets/icon.svg";
+import "../../assets/icon-192x192.png";
+import "../../assets/icon-512x512.png";
 import "../../assets/manifest.webmanifest";
-import "../../assets/mstile-150x150.png";
-import "../../assets/safari-pinned-tab.svg";
-import vars from "!css-variables-loader!../../theme/vars.css";
 import { version } from "../../../package.json";
+import { subtitle, title } from "../../scripts/data";
+import vars from "!css-variables-loader!../../theme/vars.css";
 
 const themeColor = vars["--theme-color"];
+
+const schema = {
+  "@context": "https://schema.org",
+  "@graph": [
+    {
+      "@type": "WebSite",
+      name: `${title} | ${subtitle}`,
+      alternateName: title,
+      url: "https://scottboyle.uk",
+    },
+    {
+      "@type": "Person",
+      name: title,
+      sameAs: [
+        "https://github.com/monospaced",
+        "https://www.linkedin.com/in/scottboyle/",
+      ],
+      url: "https://scottboyle.uk",
+    },
+  ],
+};
 
 const Head = ({ meta }) => {
   return (
     <head>
       <meta charSet="utf-8" />
       {meta}
-      <meta name="msapplication-TileColor" content={themeColor} />
       <meta name="theme-color" content={themeColor} />
       <meta name="viewport" content="width=device-width" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-touch-icon.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <link rel="manifest" href="/manifest.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color={themeColor} />
       <link
         rel="alternate"
         type="application/rss+xml"
         title="Pinboard (monospaced)"
         href="https://feeds.pinboard.in/rss/secret:408c7b80a1d865c97e0a/u:monospaced/"
       />
+      <link rel="icon" href="/favicon.ico" sizes="32x32" />
+      <link rel="icon" href="/icon.svg" type="image/svg+xml" />
+      <link
+        rel="apple-touch-icon"
+        href="/apple-touch-icon.png"
+        sizes="180x180"
+      />
+      <link rel="manifest" href="/manifest.webmanifest" />
       <link rel="stylesheet" href={`/styles.css?v=${version}`} />
+      <script
+        type="application/ld+json"
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(schema) }}
+      />
     </head>
   );
 };
